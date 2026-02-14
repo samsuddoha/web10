32273dba62ba6eb476803516fdb4b79212b4b1d9
MSD <mdsamsuddoha@Mds-MacBook-Pro-2.local>
2026-02-14 13:53:37 +0600

    p

--- exam/democode/complaint_system/includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "complaint_system";
-
-$conn = new mysqli($host, $user, $pass, $db);
-if (!$conn) die("DB connection failed: " . mysqli_connect_error());
-
